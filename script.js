f8973d08de3fd11a2255f473bd7a5854ec9d510d
Aditya Raj <154418353+Codeadityaa@users.noreply.github.com>
2026-09-17 15:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const sounds = [
+    "applause",
+    "boo",
+    "gasp",
+    "tada",
+    "victory",
+    "wrong",
+    "stop"
+];
+
+const buttons = document.getElementById("buttons");
+
+sounds.forEach(function(sound) {
+
+    const button = document.createElement("button");
+
+    if (sound === "stop") {
+        button.classList.add("stop");
+    } else {
+        button.classList.add("btn");
+    }
+
+    button.textContent = sound;
+
+    buttons.appendChild(button);
+});
